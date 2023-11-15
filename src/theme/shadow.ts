75969f60bb367bcc7ddbefefb1ee828918e05045
Aurelien Borst <aurelien.borst@gmail.com>
2023-11-16 00:51:18 +0100

    add shadows

--- src/theme/shadow.ts
+++ src/theme/shadow.ts
@@ -31,13 +31,45 @@ export const shadow = {
   },
   sm: {
     shadowColor: color.black,
-    shadowOffset: {
-      width: 0,
-      height: 12,
-    },
-    shadowOpacity: 0.58,
-    shadowRadius: 16.0,
+    shadowOffset: { width: 0, height: 12 },
+    shadowOpacity: 0.1,
+    shadowRadius: 12,
+    elevation: 12,
+  },
+  md: {
+    shadowColor: color.black,
+    shadowOffset: { width: 0, height: 16 },
+    shadowOpacity: 0.1,
+    shadowRadius: 16,
+    elevation: 16,
+  },
+  lg: {
+    shadowColor: color.black,
+    shadowOffset: { width: 0, height: 24 },
+    shadowOpacity: 0.15,
+    shadowRadius: 24,
     elevation: 24,
+  },
+  xl: {
+    shadowColor: color.black,
+    shadowOffset: { width: 0, height: 32 },
+    shadowOpacity: 0.15,
+    shadowRadius: 32,
+    elevation: 32,
+  },
+  xxl: {
+    shadowColor: color.black,
+    shadowOffset: { width: 0, height: 40 },
+    shadowOpacity: 0.2,
+    shadowRadius: 40,
+    elevation: 40,
+  },
+  xxxl: {
+    shadowColor: color.black,
+    shadowOffset: { width: 0, height: 48 },
+    shadowOpacity: 0.25,
+    shadowRadius: 48,
+    elevation: 48,
   },
   // ... Continue with md, lg, xl, xxl, xxxl
 };
