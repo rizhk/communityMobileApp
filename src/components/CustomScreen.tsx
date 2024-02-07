b1c375315032a69ac952ab5a9719a7a0efcc78ac
Aurelien Borst <aurelien.borst@gmail.com>
2024-02-07 20:02:05 +0100

    add customScreen

---
+++ src/components/CustomScreen.tsx
@@ -0,0 +1,41 @@
+// CustomScreen.js
+import React from "react";
+import { SafeAreaView, ScrollView } from "react-native";
+import { Button } from "components/Button";
+import { Stack, YStack } from "components/containers";
+import { LeftArrow } from "assets/svg";
+import { formatDateFromToday } from "utils/Date";
+import { Text } from "components/Text";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { MainStackParamList } from "navigators/MainStack/MainNavProps";
+
+const CustomScreen = ({
+  navigation,
+  children,
+}: {
+  navigation: NativeStackNavigationProp<MainStackParamList>;
+  children: React.ReactNode;
+}) => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <YStack flex={1} pa={"md"} pt="xxxl">
+          <Button
+            icon={LeftArrow}
+            iconScale={3}
+            rounded
+            onPress={() => {
+              navigation.goBack();
+            }}
+          />
+
+          {/* <Stack h={1} bc="grey600"></Stack> */}
+
+          {children}
+        </YStack>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default CustomScreen;
--- src/screens/ActualityScreen/ActualityScreen.tsx
+++ src/screens/ActualityScreen/ActualityScreen.tsx
@@ -11,6 +11,7 @@ import { ScrollView } from "react-native-gesture-handler";
 
 import { UserItem } from "types/user";
 import { ActualityItem } from "types/actuality";
+import CustomScreen from "components/CustomScreen";
 
 type Props = NativeStackScreenProps<MainStackParamList, "actuality">;
 
@@ -18,58 +19,26 @@ export function ActualityScreen({ navigation, route }: Props) {
   const { startDate, content, title }: ActualityItem = route.params.actuality;
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <YStack flex={1} pa={"md"} mt="xxxl">
-          <Button
-            onPress={() => {
-              navigation.goBack();
-              // console.log("ysaad");
-            }}
-            icon={LeftArrow}
-            iconScale={3}
-            rounded
-          />
-
-          {/* <Stack jc="center" ai="center" h={200}>
-            <Image source={{ uri: sport.icon.url }} resizeMode="contain" style={iconStyle} />
-          </Stack> */}
-          <YStack h={150} jc="space-evenly">
-            {/* <YStack jc="center">
-              <AddressField
-                textProps={{ size: "sm" }}
-                coord={coord}
-                format={"%city% (%state%), %street% %streetNb%"}
-                color="white"
-              />
-              {distance ? (
-                <Text>{distance}</Text>
-              ) : (
-                <Stack ai="flex-start">
-                  <ActualityIndicator animating={true} hidesWhenStopped={true} />
-                </Stack>
-              )}
-            </YStack> */}
-            <YStack jc="center">
-              <Text color="grey500" preset="bold">
-                {formatDateFromToday(startDate, "dd MMMM yyy")}
-              </Text>
-              <Text color="primary" size="lg" preset="bold">
-                {title}
-              </Text>
-            </YStack>
-          </YStack>
-          <Stack h={1} bc="grey600"></Stack>
-
-          {content && (
-            <YStack mt={10}>
-              <Text preset="bold" size="md" style={{ marginBottom: 10 }} text="Info : " />
-              <Text style={{ marginBottom: 10 }} color="dim" text={content} />
-              <Stack h={1} bc="grey600"></Stack>
-            </YStack>
-          )}
+    <CustomScreen navigation={navigation}>
+      <YStack h={150} jc="space-evenly">
+        <YStack jc="center">
+          <Text color="grey500" preset="bold">
+            {formatDateFromToday(startDate, "dd MMMM yyy")}
+          </Text>
+          <Text color="primary" size="lg" preset="bold">
+            {title}
+          </Text>
         </YStack>
-      </ScrollView>
-    </SafeAreaView>
+      </YStack>
+      <Stack h={1} bc="grey600"></Stack>
+
+      {content && (
+        <YStack mt={10}>
+          <Text preset="bold" size="md" style={{ marginBottom: 10 }} text="Info : " />
+          <Text style={{ marginBottom: 10 }} color="dim" text={content} />
+          <Stack h={1} bc="grey600"></Stack>
+        </YStack>
+      )}
+    </CustomScreen>
   );
 }
