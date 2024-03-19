a1b78b307e0539a5dadfe50066165fe87d76ea50
Aurelien Borst <aurelien.borst@gmail.com>
2024-03-19 17:52:45 +0100

    renaming to bottom navigation

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,9 +1,9 @@
-//Main Navigation
-export { HomeScreen } from "screens/main/HomeScreen/HomeScreen";
-export { CalendarScreen } from "screens/main/CalendarScreen/CalendarScreen";
-export { MapScreen } from "screens/main/MapScreen/MapScreen";
-export { OfficialScreen } from "screens/main/OfficialScreen/OfficialScreen";
-export { InfosScreen } from "screens/main/InfosScreen/InfosScreen";
+//Bottom Navigation
+export { HomeScreen } from "screens/bottom/HomeScreen/HomeScreen";
+export { CalendarScreen } from "screens/bottom/CalendarScreen/CalendarScreen";
+export { MapScreen } from "screens/bottom/MapScreen/MapScreen";
+export { OfficialScreen } from "screens/bottom/OfficialScreen/OfficialScreen";
+export { InfosScreen } from "screens/bottom/InfosScreen/InfosScreen";
 
 //Stack Navigation
 export { ActivityScreen } from "screens/stack/ActivityScreen/ActivityScreen";
