d7aa9359c2917c75c5b0014a56bd60a015161b6f
Aurelien Borst <aurelien.borst@gmail.com>
2024-03-21 16:39:49 +0100

    cean screewn

--- src/screens/stack/ActualityScreen/ActualityScreen.tsx
+++ src/screens/stack/ActualityScreen/ActualityScreen.tsx
@@ -1,25 +1,16 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { Button } from "components/Button";
 import { Stack, YStack } from "components/containers";
 import { MainStackParamList } from "navigators/MainStack/MainNavProps";
-import { ImageStyle, SafeAreaView, StyleProp, Touchable, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native";
 import { Text } from "components/Text";
-import { LeftArrow } from "assets/svg";
-
-import { formatDateFromToday } from "utils/Date";
-import { ScrollView } from "react-native-gesture-handler";
-
-import { UserItem } from "types/user";
 import { ActualityItem } from "types/actuality";
-import CustomScreen from "components/CustomScreen";
-import { createEditorJsViewer } from "editorjs-viewer-native";
 import EditorJsParser from "components/EditorJsParser";
 import { formatDate } from "utils/helper";
 
 type Props = NativeStackScreenProps<MainStackParamList, "actuality">;
 
 export function ActualityScreen({ navigation, route }: Props) {
-  const { content, title, contentRTE, publishedAt }: ActualityItem = route.params.actuality;
+  const { title, contentRTE, publishedAt }: ActualityItem = route.params.actuality;
 
   return (
     <SafeAreaView>
@@ -32,14 +23,6 @@ export function ActualityScreen({ navigation, route }: Props) {
           {contentRTE && <EditorJsParser content={contentRTE} />}
         </YStack>
       </YStack>
-
-      {content && (
-        <YStack mt={10}>
-          <Text preset="bold" size="md" style={{ marginBottom: 10 }} text="Info : " />
-          <Text style={{ marginBottom: 10 }} color="dim" text={content} />
-          <Stack h={1} bc="grey600"></Stack>
-        </YStack>
-      )}
     </SafeAreaView>
   );
 }
